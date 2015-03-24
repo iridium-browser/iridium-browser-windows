12e89c0354fa3279619dfbe9441795b972388063
Joachim Bauch <bauch@struktur.de>
2015-03-24 16:15:54 +0100

    Fixed copying installer for 32bit build.

--- scripts/windows/100_compile_release.cmd
+++ scripts/windows/100_compile_release.cmd
@@ -21,5 +21,5 @@ if not exist "C:\vagrant\build_result\%TODAY:~0,8%-%TODAY:~8,6%" (
 if "%1" == "x64" (
     copy /y src\out\Release_x64\mini_installer.exe C:\vagrant\build_result\%TODAY:~0,8%-%TODAY:~8,6%\mini_installer_x64.exe
 ) else (
-    copy /y src\out\Release\dist\mini_installer.exe C:\vagrant\build_result\%TODAY:~0,8%-%TODAY:~8,6%\mini_installer_x86.exe
+    copy /y src\out\Release\mini_installer.exe C:\vagrant\build_result\%TODAY:~0,8%-%TODAY:~8,6%\mini_installer_x86.exe
 )
