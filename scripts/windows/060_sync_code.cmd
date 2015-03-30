25d15e3d42b014a1bfcc6bca5283aebed98b7242
Joachim Bauch <bauch@struktur.de>
2015-03-30 14:03:47 +0200

    Only fetch tag information if required tag is unknown.

--- scripts/windows/060_sync_code.cmd
+++ scripts/windows/060_sync_code.cmd
@@ -3,8 +3,11 @@ rem Checkout source code
 
 cd develop/src
 
-echo Fetching tag information...
-call git fetch --tags
+git tag -l "40.0.2214.111" | findstr "40.0.2214.111" >nul 2>&1
+if not %errorlevel% == 0 (
+    echo Fetching tag information...
+    call git fetch --tags
+)
 
 echo Switching to release branch...
 call git checkout -B iridium_release_branch tags/40.0.2214.111
