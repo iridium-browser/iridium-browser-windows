6202e374291d773c43ff7de53a0a4f2684bc2160
Joachim Bauch <bauch@struktur.de>
2015-03-30 14:10:55 +0200

    Reset local changes if release branch already exists, use variables instead of hardcoded values.

--- scripts/windows/060_sync_code.cmd
+++ scripts/windows/060_sync_code.cmd
@@ -1,16 +1,26 @@
 @echo off
 rem Checkout source code
 
+set CHROMIUM_VERSION=40.0.2214.111
+set RELEASE_BRANCH=iridium_release_branch
+
 cd develop/src
 
-git tag -l "40.0.2214.111" | findstr "40.0.2214.111" >nul 2>&1
+git tag -l "%CHROMIUM_VERSION%" | findstr "%CHROMIUM_VERSION%" >nul 2>&1
 if not %errorlevel% == 0 (
     echo Fetching tag information...
     call git fetch --tags
 )
 
-echo Switching to release branch...
-call git checkout -B iridium_release_branch tags/40.0.2214.111
+git branch --list "%RELEASE_BRANCH%" | findstr "%RELEASE_BRANCH%" >nul 2>&1
+if not %errorlevel% == 0 (
+    echo Switching to release branch...
+    call git checkout -B %RELEASE_BRANCH% tags/%CHROMIUM_VERSION%
+) else (
+    echo Resetting local changes...
+    call git reset --hard tags/%CHROMIUM_VERSION%
+    call git clean -d -f -f
+)
 
 echo Syncing source code...
 call gclient sync --with_branch_heads --nohooks
