6e7035d1937871f7f206d3bc7c58cda3fd926da2
Joachim Bauch <bauch@struktur.de>
2015-07-10 09:36:57 +0200

    Activate support for proprietary codecs.

--- scripts/windows/080_runhooks.cmd
+++ scripts/windows/080_runhooks.cmd
@@ -11,6 +11,10 @@ if "%1" == "x64" (
 set GYP_GENERATORS=ninja
 set GYP_MSVS_VERSION=2013
 
+rem Enable proprietary codecs in Iridium (this adds various mime-types but
+rem doesn't include the actual codecs).
+set GYP_DEFINES=%GYP_DEFINES% proprietary_codecs=1
+
 rem Ignore macro redefinitions for now, will get fixed upstream in iridium-browser
 set CFLAGS=%CFLAGS% /wd4005
 set CXXFLAGS=%CXXFLAGS% /wd4005
