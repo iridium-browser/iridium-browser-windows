d64a29854c8db16beff0f771815a6ab0fe748e2b
Joachim Bauch <bauch@struktur.de>
2015-07-10 09:37:41 +0200

    Compile ffmpegsumo both as branding "Chromium" and "Chrome".

--- scripts/windows/100_compile_release.cmd
+++ scripts/windows/100_compile_release.cmd
@@ -16,3 +16,25 @@ if exist %RELEASE_FOLDER%\*.manifest (
     del /q %RELEASE_FOLDER%\*.manifest
 )
 ninja -C %RELEASE_FOLDER% chrome.7z
+if not %errorlevel% == 0 (
+    exit /b 1
+)
+
+echo Building FFmpeg with free codecs
+set OLD_GYP_DEFINES=%GYP_DEFINES%
+set GYP_DEFINES=%OLD_GYP_DEFINES% ffmpeg_branding=Chromium
+call python src\build\gyp_chromium src\third_party\ffmpeg\ffmpeg.gyp
+ninja -C %RELEASE_FOLDER% ffmpegsumo
+if not %errorlevel% == 0 (
+    exit /b 1
+)
+copy /y %RELEASE_FOLDER%\ffmpegsumo.dll %RELEASE_FOLDER%\ffmpegsumo-free.dll >NUL
+
+echo Building FFmpeg with proprietary codecs
+set GYP_DEFINES=%OLD_GYP_DEFINES% ffmpeg_branding=Chrome
+call python src\build\gyp_chromium src\third_party\ffmpeg\ffmpeg.gyp
+ninja -C %RELEASE_FOLDER% ffmpegsumo
+if not %errorlevel% == 0 (
+    exit /b 1
+)
+copy /y %RELEASE_FOLDER%\ffmpegsumo.dll %RELEASE_FOLDER%\ffmpegsumo-extra.dll >NUL
